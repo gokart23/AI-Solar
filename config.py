c4e6a19b65ee673e53b85d70702b2826c646181e
jayadeepk <karnatijayadeep@gmail.com>
2017-04-15 22:59:58 +0530

    Drop specific columns while reading csv
    Add method for combining all cities' data with additional
    column named 'city' indexed 0-4.

--- config.py
+++ config.py
@@ -6,3 +6,15 @@ directories = {
     'Guwahati': 'data/NREL-Guwahati-2000-2014',
     'Mumbai': 'data/NREL-Mumbai-2000-2014',
 }
+
+# Features to be dropped while reading csv data
+dropped_features = [
+    0,      # Year
+    4,      # Minute
+    8,      # ClearSky DHI
+    9,      # Clearsky DNI
+    10,     # Clearsky GHI
+    17,     # Snow Depth
+    18,     # Wind Direction
+    20,     # Fill Flag
+]
--- data.py
+++ data.py
@@ -1,11 +1,13 @@
+import config
 import os
 import pandas as pd
 
 
 def read_csv(file_path):
     """Read NREL csv file into data frame"""
+    cols = [x for x in range(21) if x not in config.dropped_features]
     return pd.read_csv(file_path, header=0, index_col=False,
-                       usecols=list(range(5, 20)), skiprows=2)
+                       usecols=cols, skiprows=2)
 
 
 def read_csv_directory(directory_path):
@@ -17,3 +19,21 @@ def read_csv_directory(directory_path):
         df = read_csv(file_path)
         data_frames.append(df)
     return pd.concat(data_frames, ignore_index=True)
+
+
+def read_csv_all_cities():
+    """Read NREL csv files for every city and concatenate with additional city index"""
+    data_frames = []
+    city_index = 0
+    for city in sorted(config.directories.keys()):
+        path = config.directories[city]
+        df = read_csv_directory(path)
+
+        # Create new column with city index and append it to data frame
+        new_column = pd.DataFrame([city_index]*df.shape[0])
+        df = df.assign(city=new_column)
+
+        data_frames.append(df)
+        city_index += 1
+    return pd.concat(data_frames, ignore_index=True)
+
