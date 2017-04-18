7b72842d6c4990930e8c735287d262c1f9939678
jayadeepk <karnatijayadeep@gmail.com>
2017-04-18 06:52:00 +0530

    Format code with no logic changes
    Combined the methods `read_csv_directory` and `read_csv_all_city` into
    `read_raw_data` to avoid confusion. The method reads local or global data
    based on city argument.
    
    `preprocess` method now accepts city argument for preprocessing specific
    city's data and saves it to data/preprocessed_<city>.pkl.
    
    `read_data` method now accepts city argument for reading preprocessed
    data of specific city.
    
    Removed trailing spaces in few lines.
    Changed tabs to spaces in `linear_models.py`. Reformatted redundant code.
    
    Saved baseline model results for each city into corresponding files.

--- data.py
+++ data.py
@@ -1,9 +1,8 @@
 import config
-import os
 import numpy as np
+import os
 import pandas as pd
 import _pickle as pkl
-import sklearn.model_selection as skm
 from sklearn.preprocessing import MinMaxScaler
 
 
@@ -14,24 +13,29 @@ def read_csv(file_path):
                        usecols=cols, skiprows=2)
 
 
-def read_csv_directory(directory_path):
-    """Read NREL csv files from directory into data frame and concatenate."""
-    files = os.listdir(directory_path)
-    data_frames = []
-    for file in sorted(files):
-        file_path = os.path.join(directory_path, file)
-        df = read_csv(file_path)
-        data_frames.append(df)
-    return pd.concat(data_frames, ignore_index=True)
+def read_raw_data(city=''):
+    """Read NREL csv files into data frame and concatenate
 
+    If city is not specified, concatenate data of all cities and append
+    a new column containing city indices.
+    
+    :param city: City name string
+    :return: Raw data DataFrame
+    """
+    if city:
+        files = os.listdir(config.directories[city])
+        data_frames = []
+        for file in sorted(files):
+            file_path = os.path.join(config.directories[city], file)
+            df = read_csv(file_path)
+            data_frames.append(df)
+        return pd.concat(data_frames, ignore_index=True)
 
-def read_csv_all_cities():
-    """Read NREL csv files for every city and concatenate with additional city index"""
+    # Concatenate data of all cities with additional city index
     data_frames = []
     city_index = 0
-    for city in sorted(config.directories.keys()):
-        path = config.directories[city]
-        df = read_csv_directory(path)
+    for city_ in sorted(config.cities):
+        df = read_raw_data(city_)
 
         # Create new column with city index and append it to data frame
         new_column = pd.DataFrame([city_index]*df.shape[0])
@@ -56,15 +60,22 @@ def save_scaler_extrema(minimum, maximum, columns):
     dfmax.to_pickle('data/norm_extrema/norm_max.pkl')
 
 
-def preprocess():
-    """Read raw csv data, preprocess and save to data/preprocessed.pkl"""
-    df = read_csv_all_cities()
+def preprocess(city=''):
+    """Read raw raw data, preprocess and save to file
+    
+    If city is specified, read raw data of the city and save preprocessed
+    data to data/preprocessed_<city>.pkl. Else, read raw data of all
+    cities and save to data/preprocessed.pkl.
+    """
+    df = read_raw_data(city)
 
     # Drop night time hours
     df = df[~df.Hour.isin(config.dropped_hours)].reset_index(drop=True)
 
     # Convert hour, day, month and city into one-hot vectors
-    onehot = ['Hour', 'Day', 'Month', 'City']
+    onehot = ['Hour', 'Day', 'Month']
+    if not city:
+        onehot.append('City')
 
     dummified = pd.get_dummies(df, columns=onehot, prefix=onehot, prefix_sep='')
     column_names = dummified.columns
@@ -82,49 +93,54 @@ def preprocess():
                            ignore_index=True)
     normalized.columns = column_names
 
-    normalized.to_pickle('data/preprocessed.pkl')
+    save_path = 'data/preprocessed_' + city + '.pkl' if city else 'data/preprocessed.pkl'
+    normalized.to_pickle(save_path)
+    print('Saved preprocessed data to: ' + save_path)
 
 
-def read_data():
+def read_data(city=''):
     """Read pickle containing preprocessed data"""
+    if city:
+        return pd.read_pickle('data/preprocessed_' + city + '.pkl')
     return pd.read_pickle('data/preprocessed.pkl')
+
 
 def input_output_split(df):
     """Separate input variables and output variables of data"""
     return df.ix[:, 'Dew Point':], df.ix[:, :'GHI']
 
+
 def train_validate_test_split(df, test_percent=.2, validate_percent=.25, seed=2345):
     """Generate training/validation/testing data uniformly from dataset"""
     np.random.seed(seed)
     perm = np.random.permutation(df.index)
-        
-    test_end = int( (test_percent) * df.shape[0])
-    test = df.ix[ perm[:test_end] ]    
-    
-    validate_end = int( (1-test_percent) * validate_percent * df.shape[0]) + test_end
+
+    test_end = int(test_percent * df.shape[0])
+    test = df.ix[perm[:test_end]]
+
+    validate_end = int((1-test_percent) * validate_percent * df.shape[0]) + test_end
     validate = df.ix[perm[test_end:validate_end]]
-    
+
     train = df.ix[perm[validate_end:]]
-    
+
     return train, validate, test
 
-def create_dataset_global(savefile="dataset"):
-    """Generate training, validation and testing data from all data"""
-    df = read_data()
-    df_train, df_val, df_test = train_validate_test_split(df)
-    (X_train, y_train), (X_val, y_val), (X_test, y_test) = input_output_split(df_train), input_output_split(df_val), input_output_split(df_test)
-    
-    # Not using to_pickle or np.savez in order to maintain DF structure
-    with open("data/" + savefile + ".pkl", "wb") as f:
-        pkl.dump( dict(X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, X_test=X_test, y_test=y_test), f)
 
-def create_dataset_local(city, savefile="dataset"):
-    """Generate training, validation and testing data for a city"""
-    df = read_csv_directory(config.directories[city])
+def create_dataset(city='', savefile='dataset'):
+    """Generate training, validation and testing data from preprocessed data
+    
+    If city is not specified, generate for all cities.
+    """
+    df = read_data(city)
     df_train, df_val, df_test = train_validate_test_split(df)
     (X_train, y_train), (X_val, y_val), (X_test, y_test) = input_output_split(df_train), input_output_split(df_val), input_output_split(df_test)
-    with open("data/" + savefile + "_" + city + ".pkl", "wb") as f:
+
+    # Not using to_pickle or np.savez in order to maintain DF structure
+    save_path = 'data/' + savefile + '_' + city + '.pkl' if city else 'data/' + savefile + '.pkl'
+    with open(save_path, 'wb') as f:
         pkl.dump( dict(X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, X_test=X_test, y_test=y_test), f)
+    print('Saved generated dataset to: ' + save_path)
+
 
 def load_dataset(savefile="data/dataset.pkl"):
     """Load the training, validation and testing data from saved file"""
@@ -133,11 +149,14 @@ def load_dataset(savefile="data/dataset.pkl"):
 
     return data['X_train'],data['y_train'], data['X_val'], data['y_val'], data['X_test'], data['y_test']
 
-if __name__ == '__main__':
-    # preprocess()
 
-    create_dataset_global()
-    for city in config.directories.keys():
-        create_dataset_local(city)
+if __name__ == '__main__':
+    preprocess()
+    for city in config.cities:
+        preprocess(city)
+
+    create_dataset()
+    for city in config.cities:
+        create_dataset(city)
 
     X_train, y_train, X_val, y_val, X_test, y_test = load_dataset(savefile=config.datasets['Delhi'])
--- linear_models.py
+++ linear_models.py
@@ -1,59 +1,72 @@
 import data
 import config
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 import sklearn.metrics as metrics
 
-def baseline_model(savefile="baseline_model", city=None):
-	""" Train a un-normalized linear regression model and obtain predictions on test-set """
 
-	if city == None:
-		X_train, y_train, X_val, y_val, X_test, y_test = data.load_dataset()
-	else:
-		X_train, y_train, X_val, y_val, X_test, y_test = data.load_dataset(config.datasets[city])
+def baseline_model(city='', savefile="models.baseline_model"):
+    """Train an un-normalized linear regression model and obtain predictions on test-set"""
 
-	
+    if not city:
+        X_train, y_train, X_val, y_val, X_test, y_test = data.load_dataset()
+    else:
+        X_train, y_train, X_val, y_val, X_test, y_test = data.load_dataset(config.datasets[city])
 
-	regr_lm = linear_model.LinearRegression()
+    regr_lm = linear_model.LinearRegression()
 
-	print("Fitting model")
-	regr_lm.fit(X_train, y_train)
-	# print('Coefficients shape: \n', regr_lm.coef_.shape)
+    if city:
+        print('\nFitting model for ' + city)
+    else:
+        print("\nFitting global model")
+    regr_lm.fit(X_train, y_train)
+    # print('Coefficients shape: \n', regr_lm.coef_.shape)
 
-	print("Predicting values")
-	pred_val = regr_lm.predict(X_val)
-	pred_test = regr_lm.predict(X_test)
+    print("Predicting values")
+    pred_val = regr_lm.predict(X_val)
+    pred_test = regr_lm.predict(X_test)
 
-	op_file = open(config.output_directory + "models." + savefile + ".txt", "w+")
-	op_file.write("Baseline Model - Un-normalized Linear Regression\n\n")
+    op_path = config.output_directory + savefile
+    op_path += '-' + city + '.txt' if city else '.txt'
+    op_file = open(op_path, "w+")
+    op_file.write("Baseline Model - Un-normalized Linear Regression\n\n")
+
+    # The root-mean-squared error (RMSE) on validation data
+    rmse_val = np.sqrt(metrics.mean_squared_error(y_val, pred_val))
+    print_write("Mean squared error(validation): " + str(rmse_val), op_file)
+
+    # R2 coefficient on validation data: 1 is perfect prediction
+    r2_val = metrics.r2_score(y_val, pred_val, multioutput='variance_weighted')
+    print_write("R2 score(validation): " + str(r2_val), op_file)
+
+    # The root-mean-squared error (RMSE) on test data
+    rmse_test = np.sqrt(metrics.mean_squared_error(y_test, pred_test))
+    print_write("Mean squared error(test): " + str(rmse_test), op_file)
+
+    # R2 coefficient on test data: 1 is perfect prediction
+    r2_test = metrics.r2_score(y_test, pred_test, multioutput='variance_weighted')
+    print_write('R2 score(test): ' + str(r2_test), op_file)
+
+    op_file.close()
 
 
-	# The root-mean-squared error (RMSE)
-	print("Mean squared error(validation): ", np.sqrt(metrics.mean_squared_error(y_val, pred_val)) )
-	op_file.write( ("Mean squared error(validation): " + str(np.sqrt(metrics.mean_squared_error(y_val, pred_val))) + "\n") )
-	# R2 coefficient: 1 is perfect prediction
-	print("R2 score(validation): " , metrics.r2_score(y_val, pred_val, multioutput='variance_weighted'))
-	op_file.write( ("R2 score(validation): " + str(metrics.r2_score(y_val, pred_val,  multioutput='variance_weighted')) + "\n") )
+def print_write(line, f):
+    """Print line and write line to file"""
+    print(line)
+    f.write(line + '\n')
 
-	print("Mean squared error(test): ",
-				 np.sqrt(metrics.mean_squared_error(y_test, pred_test)))
-	op_file.write( ("Mean squared error(test): " +
-				 str(np.sqrt(metrics.mean_squared_error(y_test, pred_test))) + "\n") )
-	print("R2 score(test): " , metrics.r2_score(y_test, pred_test, multioutput='variance_weighted'))
-	op_file.write( ("R2 score(test): " + str(metrics.r2_score(y_test, pred_test, multioutput='variance_weighted')) + "\n") )
 
-	op_file.close()
-
-	# TODO: Generate figures for visual comparison of pred and actual values
-	# fig = plt.figure()
-	# plt.plot(range(y_test.shape[0]), pred_test[:, 0], '--')
-	# plt.plot(range(y_test.shape[0]), y_test.ix[:, 0], '--')
-	# plt.xlabel('Index')
-	# plt.ylabel('Direct Horizontal Irradiance (W/m^2)')
-	# plt.savefig("trial.png")
-	# plt.show()
+# TODO: Generate figures for visual comparison of pred and actual values
+# fig = plt.figure()
+# plt.plot(range(y_test.shape[0]), pred_test[:, 0], '--')
+# plt.plot(range(y_test.shape[0]), y_test.ix[:, 0], '--')
+# plt.xlabel('Index')
+# plt.ylabel('Direct Horizontal Irradiance (W/m^2)')
+# plt.savefig("trial.png")
+# plt.show()
 
 if __name__ == "__main__":
-	baseline_model()
+    baseline_model()
+    for city in config.cities:
+        baseline_model(city)
