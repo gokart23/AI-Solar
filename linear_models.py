ca4adfa0837318e37639b699d374dc7952bac884
Karthik Duddu <kar.meher95@gmail.com>
2017-04-19 16:34:30 +0400

    Added Lasso model. Achieved global R2 of 0.72

--- linear_models.py
+++ linear_models.py
@@ -1,6 +1,7 @@
 import data
 import config
 import metrics
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 
@@ -29,17 +30,68 @@ def baseline_model(city='', savefile="models.baseline_model"):
     metrics.generate_results(city, y_val, y_test, pred_val, pred_test, savefile, results_heading)
 
 
-# TODO: Generate figures for visual comparison of pred and actual values
-# fig = plt.figure()
-# plt.plot(range(y_test.shape[0]), pred_test[:, 0], '--')
-# plt.plot(range(y_test.shape[0]), y_test.ix[:, 0], '--')
-# plt.xlabel('Index')
-# plt.ylabel('Direct Horizontal Irradiance (W/m^2)')
-# plt.savefig("trial.png")
-# plt.show()
+def lasso_model(city='', max_degree=4, savefile="models.lasso_model"):
+    """Train a linear regression model with L1-regularization (and polynomial kernel),
+       validate according to alpha, and obtain predictions on test-set"""
+
+    if not city:
+        X_train, y_train, X_val, y_val, X_test, y_test = data.load_dataset()
+    else:
+        X_train, y_train, X_val, y_val, X_test, y_test = data.load_dataset(config.datasets[city])
+
+    for cur_degree in range(2, max_degree):
+        for dataset in [X_train, X_val, X_test]:
+            dataset = np.hstack( (dataset, np.power(dataset, cur_degree) ) )
+
+    if city:
+        print('\nFitting model for ' + city)
+    else:
+        print("\nFitting global model")
+
+    
+    max_alpha, max_r2 = 0, 0
+    for alpha in [0.00001, 0.0001, 0.0005, 0.001, 0.01, 0.1]:
+        regr_lm = linear_model.Lasso(alpha=alpha, max_iter=1e6)
+
+        regr_lm.fit(X_train, y_train)
+        print("Predicting values on", alpha)
+        pred_val = regr_lm.predict(X_val)
+        pred_test = regr_lm.predict(X_test)
+
+        results_heading = 'Baseline Model - Linear Regression with L1-regularization'
+        results_heading += "\nCurrent alpha:" + str(alpha)
+
+        rmse_val, r2_val, _, _ = metrics.generate_results(city, y_val, y_test, pred_val, pred_test, savefile + "_" + str(alpha), results_heading)
+        max_alpha, max_r2 = (alpha if max_r2 < r2_val else max_alpha), (r2_val if max_r2 < r2_val else max_r2)
+    
+    # TODO: Change this to just rename instead of recompute
+    print ("Best model observed on validation for alpha", max_alpha, "with score", max_r2)
+    regr_lm = linear_model.Lasso(alpha=max_alpha, max_iter=1e8)
+
+    regr_lm.fit(X_train, y_train)
+    print("Predicting values on best model")
+    pred_val = regr_lm.predict(X_val)
+    pred_test = regr_lm.predict(X_test)
+
+    results_heading = 'Baseline Model - Linear Regression with L1-regularization'
+    results_heading += "\nCurrent alpha:" + str(alpha)
+
+    metrics.generate_results(city, y_val, y_test, pred_val, pred_test, savefile + "_best", results_heading)
+
+    plt.figure()
+    plt.plot(regr_lm.coef_, color='gold', linewidth=2, label='Lasso coefficients')
+    plt.savefig(savefile + "_best_coef_plot.png")
+    plt.close()
+    
+    return max_alpha, max_r2
+
+def get_model_results(model):
+    """ Function to run the specified model on all default parameters on global and local datasets"""
+    model()
+    for city_ in config.cities:
+        model(city_)
 
 if __name__ == "__main__":
-    baseline_model()
-    for city_ in config.cities:
-        baseline_model(city_)
+    # get_model_results(baseline_model)
 
+    get_model_results(lasso_model)
--- metrics.py
+++ metrics.py
@@ -3,7 +3,7 @@ import numpy as np
 import sklearn.metrics as metrics
 
 
-def generate_results(city, y_val, y_test, pred_val, pred_test, savefile, heading):
+def generate_results(city, y_val, y_test, pred_val, pred_test, savefile, heading, skip_save=False):
     """Calculate rmse and r2 score for validation and testing"""
     op_path = config.output_directory + savefile
     op_path += '-' + city + '.txt' if city else '.txt'
@@ -12,21 +12,35 @@ def generate_results(city, y_val, y_test, pred_val, pred_test, savefile, heading
 
     # The root-mean-squared error (RMSE) on validation data
     rmse_val = np.sqrt(metrics.mean_squared_error(y_val, pred_val))
-    print_write("Mean squared error(validation): " + str(rmse_val), op_file)
+    if skip_save == True:
+        print("Mean squared error(validation): " + str(rmse_val), op_file)
+    else:
+        print_write("Mean squared error(validation): " + str(rmse_val), op_file)
 
     # R2 coefficient on validation data: 1 is perfect prediction
     r2_val = metrics.r2_score(y_val, pred_val, multioutput='variance_weighted')
-    print_write("R2 score(validation): " + str(r2_val), op_file)
+    if skip_save == True:
+        print("R2 score(validation): " + str(r2_val), op_file)
+    else:
+        print_write("R2 score(validation): " + str(r2_val), op_file)
 
     # The root-mean-squared error (RMSE) on test data
     rmse_test = np.sqrt(metrics.mean_squared_error(y_test, pred_test))
-    print_write("Mean squared error(test): " + str(rmse_test), op_file)
+    if skip_save == True:
+        print("Mean squared error(test): " + str(rmse_test), op_file)
+    else:
+        print_write("Mean squared error(test): " + str(rmse_test), op_file)
 
     # R2 coefficient on test data: 1 is perfect prediction
     r2_test = metrics.r2_score(y_test, pred_test, multioutput='variance_weighted')
-    print_write('R2 score(test): ' + str(r2_test), op_file)
+    if skip_save == True:
+        print('R2 score(test): ' + str(r2_test), op_file)
+    else:
+        print_write('R2 score(test): ' + str(r2_test), op_file)
 
     op_file.close()
+
+    return rmse_val, r2_val, rmse_test, r2_test
 
 
 def print_write(line, f):
